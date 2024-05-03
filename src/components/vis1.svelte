565c8dbb22d382e91a1715080dd5c1e1f44e212b
larann901 <145437746+larann901@users.noreply.github.com>
2024-05-03 17:09:59 +0200

    Add files via upload

--- src/components/vis1.svelte
+++ src/components/vis1.svelte
@@ -1,7 +1,34 @@
+<style>
+    .plot {
+        top: 50%;
+        left: 50%;
+    }
+
+        .circle:hover {
+            fill-opacity : 0.6;
+            content: attr(data-tooltip); /* Set tooltip content */
+        }
+        
+        .outside-text {
+            position: absolute;
+            top: 0px; /* Adjust top position */
+            left: 0px; /* Adjust left position */
+        }
+
+        svg text {
+            display: none; /* Hide all text elements by default */
+        }
+
+        svg g:hover text {
+            display: block; /* Display text elements when circle is hovered over*/
+        }
+
+</style>
+
 <script>
     import data_vis1 from '../data/vis1_coordinate.json';
     
-    import Button from './Button.svelte';
+    import Button from './Button.svelte';   
 
     let datapoints_vis1 = data_vis1.map(d => {
       return {
@@ -13,7 +40,7 @@
       }
     });
 
-    let rescaling_factor = 0.3;
+    let rescaling_factor = 0.25;
 
     let map_width = 2890 * rescaling_factor;
     let map_height = 1940 * rescaling_factor;
@@ -46,31 +73,41 @@
             slider_value = i;
         }, i * 500); // Wait 500 iterations, then update slider_value
     }
+
 }
 
 </script>
+
 <body>
+
+<h1 style="position: absolute; top: 5px; left: 10px;"> Visualization 1 </h1>
+
 <!--<input type="range" min="201901" max="202312" bind:value={slider_value} /><br/>-->
-<input type="range" min="0" max="59" step="1" bind:value={slider_value} />
+<div style="position: absolute; top: 90%; left: 30%; white-space: nowrap">
+    <input style="position: absolute; top: 0px; left: 60px;" type="range" min="0" max="59" step="1" bind:value={slider_value} />
+    <p style="position: absolute; top: -17px; left: 240px; margin-top: 20px;">Date: {mapSliderToDateString(slider_value)} </p>
+    <Button style="position: absolute; top: 0px; left: 0; margin-top: 20px;" on:click={play_animation}>
+        Play
+    </Button>
+</div>
 
-<!--Prints the date that is being displayed-->
-<p>{mapSliderToDateString(slider_value)}</p>
-
-<!-- Button! -->
-<Button on:click={play_animation}>
-    Play
-</Button>
 
 <!--The actual plot-->
-<svg width={map_width} height={map_height}>
+<svg class = "plot" width={map_width} height={map_height}>
       <image xlink:href="http://localhost:5173/faerun.jpg" width={map_width} height={map_height} opacity="0.7"/>
         {#each datapoints_vis1 as datapoint}
         {#if datapoint.year_month == mapSliderToDateString(slider_value)}
-            <circle 
+            <g>
+            <circle class="circle"
+            data-tooltip="test"
             cx={datapoint.Coord_X * rescaling_factor} 
             cy={datapoint.Coord_Y * rescaling_factor} 
             r= {datapoint.CartPriceInCP * (0.00008 * rescaling_factor)}
-            fill-opacity = "0.4" />
+            fill-opacity = "0.4"
+            fill = #4D4D4D 
+            />
+            <text class = "outside-text" x="50" y="50">{datapoint.Nation}</text>
+            </g>
         {/if}
         {/each}
   </svg>
