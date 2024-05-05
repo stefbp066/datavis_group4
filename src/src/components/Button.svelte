79849902850c97a9c755b4ec76ec3d5f187c567e
larann901 <145437746+larann901@users.noreply.github.com>
2024-05-05 17:28:35 +0200

    Add files via upload

---
+++ src/src/components/Button.svelte
@@ -0,0 +1,34 @@
+<script>
+	
+	let buttonProps = {
+	class:[$$restProps.class]
+	}
+
+</script>
+	<button on:click
+			on:mouseover
+			on:mouseenter
+			on:mouseleave
+		{...buttonProps}>
+			<slot/>
+	</button>
+
+
+<style>
+	.play-button {
+        padding: 10px;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        transition: transform 0.9s ease;
+    }
+
+    .play-button:hover {
+        transform: scale(1.1);
+    }
+
+    .play-icon {
+        width: 240px;
+        height: 240px;
+    }
+</style>
